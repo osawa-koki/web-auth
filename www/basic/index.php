0413196d32e94fb93aff0e78c7fc72a2e653548c
osawa-koki <koki@osawa.dev>
2023-08-08 14:13:30 +0900

    Basic Auth

---
+++ www/basic/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Basic Auth</title>
+</head>
+<body>
+  <h1>Besic Auth</h1>
+  <p>Basic認証のテストです。</p>
+</body>
+</html>
